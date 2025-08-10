a878577bdfd27130408422486bdac159dc1e2f92
zihluwang <real@zihluwang.me>
2025-08-11 00:15:22 +0800

    feat: register controller

--- onixbyte-boot-server/src/main/java/com/onixbyte/onixboot/controller/AuthController.java
+++ onixbyte-boot-server/src/main/java/com/onixbyte/onixboot/controller/AuthController.java
@@ -1,4 +1,11 @@
 package com.onixbyte.onixboot.controller;
 
+import com.onixbyte.onixboot.service.WeComService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class AuthController {
 }
