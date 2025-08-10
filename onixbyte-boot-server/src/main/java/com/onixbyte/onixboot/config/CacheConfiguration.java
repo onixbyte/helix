f30d002a28a36244d95731fb6b6e76ae36bcb10d
zihluwang <real@zihluwang.me>
2025-08-11 00:15:50 +0800

    feat: customise cache manager

--- onixbyte-boot-server/src/main/java/com/onixbyte/onixboot/config/CacheConfiguration.java
+++ onixbyte-boot-server/src/main/java/com/onixbyte/onixboot/config/CacheConfiguration.java
@@ -1,9 +1,31 @@
 package com.onixbyte.onixboot.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 @Configuration
 public class CacheConfiguration {
 
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        var _keySerializer = RedisSerializer.string();
+        var _valueSerializer = new GenericJackson2JsonRedisSerializer();
 
+        var cacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(_keySerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(_valueSerializer));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(connectionFactory)
+                .cacheDefaults(cacheConfiguration)
+                .build();
+    }
 }
