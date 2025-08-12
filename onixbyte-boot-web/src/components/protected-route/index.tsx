fabd888f35e36e14fd5ada656a628a9b34236699
siujamo <jamo.siu@outlook.com>
2025-08-12 09:46:39 +0800

    feat: add protected route

---
+++ onixbyte-boot-web/src/components/protected-route/index.tsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet, useLocation } from "react-router"
+import { useAppSelector } from "@/store"
+
+export default function ProtectedRoute() {
+  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated)
+
+  const location = useLocation()
+
+  if (isAuthenticated) {
+    return <Navigate to="" state={{ from: location }} replace />
+  }
+
+  return <Outlet />
+}
