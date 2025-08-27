79263cbcb0faf48913fdbe9c52c920cfd137c66b
zihluwang <real@zihluwang.me>
2025-08-27 23:05:35 +0800

    refactor(db): add example role authority relations

--- helix-server/database/init.d/init-en_GB.sql
+++ helix-server/database/init.d/init-en_GB.sql
@@ -183,3 +183,25 @@ CREATE TABLE role_permissions
     PRIMARY KEY (role_id, permission_id)
 );
 
+INSERT INTO role_permissions
+VALUES (1, 1, CURRENT_TIMESTAMP),
+       (1, 2, CURRENT_TIMESTAMP),
+       (1, 3, CURRENT_TIMESTAMP),
+       (1, 4, CURRENT_TIMESTAMP),
+       (1, 5, CURRENT_TIMESTAMP),
+       (1, 6, CURRENT_TIMESTAMP),
+       (1, 7, CURRENT_TIMESTAMP),
+       (1, 8, CURRENT_TIMESTAMP),
+       (1, 9, CURRENT_TIMESTAMP),
+       (1, 10, CURRENT_TIMESTAMP),
+       (1, 11, CURRENT_TIMESTAMP),
+       (1, 12, CURRENT_TIMESTAMP),
+       (1, 13, CURRENT_TIMESTAMP),
+       (2, 1, CURRENT_TIMESTAMP),
+       (2, 2, CURRENT_TIMESTAMP),
+       (2, 3, CURRENT_TIMESTAMP),
+       (2, 5, CURRENT_TIMESTAMP),
+       (2, 7, CURRENT_TIMESTAMP),
+       (2, 9, CURRENT_TIMESTAMP),
+       (2, 11, CURRENT_TIMESTAMP);
+
