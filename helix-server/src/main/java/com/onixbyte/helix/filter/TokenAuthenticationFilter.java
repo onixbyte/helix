f72fbb96f4a79ffd791fb9fc738110c4bafcd190
zihluwang <real@zihluwang.me>
2025-10-21 07:54:12 +0800

    refactor: 使用 JSpecify 的 `@NonNull` 标注 Filter 参数

--- helix-server/src/main/java/com/onixbyte/helix/filter/TokenAuthenticationFilter.java
+++ helix-server/src/main/java/com/onixbyte/helix/filter/TokenAuthenticationFilter.java
@@ -10,6 +10,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.jspecify.annotations.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.GrantedAuthority;
@@ -37,9 +38,9 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
         var token = request.getHeader("Authorization");
         if (Objects.isNull(token) || token.isBlank()) {
