e9893d825c810d1259b874ec03b0861711af987f
Pho78 <linrideyouxiang@gmail.com>
2025-11-05 13:54:51 +0800

    修改：ID 必填，username 和 fullName 可以不填

--- helix-server/src/main/java/com/onixbyte/helix/domain/web/request/UpdateUserRequest.java
+++ helix-server/src/main/java/com/onixbyte/helix/domain/web/request/UpdateUserRequest.java
@@ -1,7 +1,6 @@
 package com.onixbyte.helix.domain.web.request;
 
 import com.onixbyte.helix.constant.UserStatus;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
