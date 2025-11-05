73dbd0bfb94c693381d64a447ba0491a18438d0d
Pho78 <linrideyouxiang@gmail.com>
2025-11-05 12:49:37 +0800

    修改：ID 必填，username 和 fullName 可以不填

--- helix-server/src/main/java/com/onixbyte/helix/domain/web/request/UpdateUserRequest.java
+++ helix-server/src/main/java/com/onixbyte/helix/domain/web/request/UpdateUserRequest.java
@@ -2,12 +2,15 @@ package com.onixbyte.helix.domain.web.request;
 
 import com.onixbyte.helix.constant.UserStatus;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
 import java.util.List;
 
 public record UpdateUserRequest(
+        @NotNull(message = "User ID cannot be null")
+        @Positive(message = "User ID must be positive")
         Long id,
-        @NotBlank(message = "Username cannot be empty.")
         String username,
         @NotBlank(message = "Full name cannot be empty.")
         String fullName,
