05b6bbca79234b39766be362ce4ff172eac94f06
siujamo <jamo.siu@outlook.com>
2025-12-29 12:32:40 +0800

    chore(deps): 升级 JDK 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,10 @@ tasks.withType<JavaCompile> {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
     toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
